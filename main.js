f1bc8fcf656be09f5ec5a78b0d4ba2f870b0fdf0
AugustoMarcelo <mrclgst10@gmail.com>
2020-05-23 10:54:01 -0300

    :tada: initial commit

---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron');
+
+let window;
+
+function createWindow() {
+  window = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  window.loadFile('index.html');
+
+  window.webContents.openDevTools();
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
